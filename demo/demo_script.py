29bdc991d81bc87e5b9a7555bd569f4a8c46b367
Stefan V <stefan.email.2000@gmail.com>
2024-08-22 21:35:57 +0200

    demo script only computes oMHN

--- demo/demo_script.py
+++ demo/demo_script.py
@@ -11,15 +11,15 @@ np.random.seed(0)
 
 
 def main():
-    c_opt = cMHNOptimizer()
-    c_opt.load_data_from_csv("LUAD_n12.csv")
-    c_opt.set_penalty(c_opt.Penalty.L1)
+    # c_opt = cMHNOptimizer()
+    # c_opt.load_data_from_csv("LUAD_n12.csv")
+    # c_opt.set_penalty(c_opt.Penalty.L1)
 
     o_opt = oMHNOptimizer()
     o_opt.load_data_from_csv("LUAD_n12.csv")
     o_opt.set_penalty(o_opt.Penalty.SYM_SPARSE)
 
-    data_properties = c_opt.get_data_properties()
+    data_properties = o_opt.get_data_properties()
 
     n_cv_steps = 5
     n_cv_folds = 3
@@ -28,35 +28,41 @@ def main():
     print("Minimum lambda:", lambda_min)
     print("Maximum lambda:", lambda_max)
 
-    print("cross-validate cMHN:")
-    cMHN_lambda = c_opt.lambda_from_cv(
-        lambda_min=lambda_min, lambda_max=lambda_max, steps=n_cv_steps, nfolds=n_cv_folds, show_progressbar=True)
-    print("oMHN lambda:", cMHN_lambda)
+    # print("cross-validate cMHN:")
+    # cMHN_lambda = c_opt.lambda_from_cv(
+    #     lambda_min=lambda_min, lambda_max=lambda_max, steps=n_cv_steps, nfolds=n_cv_folds, show_progressbar=True)
+    # print("cMHN lambda:", cMHN_lambda)
 
     print("cross-validate oMHN:")
     oMHN_lambda = o_opt.lambda_from_cv(
         lambda_min=lambda_min, lambda_max=lambda_max, steps=n_cv_steps, nfolds=n_cv_folds, show_progressbar=True)
     print("oMHN lambda:", oMHN_lambda)
 
-    print("Train cMHN...")
-    c_opt.train(lam=cMHN_lambda)
-    print("Save cMHN")
-    c_opt.result.save(filename="demo_script_cMHN.csv")
+    # print("Train cMHN...")
+    # c_opt.train(lam=cMHN_lambda)
+    # print("Save cMHN")
+    # c_opt.result.save(filename="demo_script_cMHN.csv")
 
     print("Train oMHN...")
     o_opt.train(lam=oMHN_lambda)
     print("Save oMHN")
     o_opt.result.save(filename="demo_script_oMHN.csv")
 
-    print("Plot cMHN")
-    c_opt.result.plot()
-    plt.savefig("cMHN_plot.png")
+    # print("Plot cMHN")
+    # c_opt.result.plot()
+    # plt.savefig("cMHN_plot.png")
+    #
+    # plt.cla()
+
+    print("Plot oMHN")
+    o_opt.result.plot(logarithmic=False)
+    plt.savefig("oMHN_plot.png")
 
     plt.cla()
 
-    print("Plot oMHN")
-    o_opt.result.plot()
-    plt.savefig("oMHN_plot.png")
+    print("Plot oMHN logarithmic")
+    o_opt.result.plot(logarithmic=True)
+    plt.savefig("oMHN_plot_log.png")
 
 
 if __name__ == '__main__':
