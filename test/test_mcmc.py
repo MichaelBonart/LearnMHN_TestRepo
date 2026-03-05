729de2283879d39351d4ffc495390da6ccf324e8
Linda Hu <hu.linda@aol.com>
2026-03-05 15:48:48 +0000

    fix

--- test/test_mcmc.py
+++ test/test_mcmc.py
@@ -2,6 +2,7 @@
 Unit tests for the mhn.mcmc module.
 """
 
+import sys
 if sys.version_info < (3, 9):
     raise unittest.SkipTest("mhn.mcmc requires Python 3.9+")
 
@@ -13,7 +14,6 @@ from mhn.mcmc.kernels import RWMKernel, MALAKernel, smMALAKernel
 from mhn.mcmc.mcmc import MCMC
 from mhn.optimizers import Penalty
 import scipy.stats
-import sys
 
 data = np.loadtxt("demo/LUAD_n12.csv", delimiter=",",
                   skiprows=1, dtype=np.int32)[:100, :3]
